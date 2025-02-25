7dd650a4a265166f1a4b9516fc08b7381f4188e9
Victor Gabriel <victor10.vg23@gmail.com>
2025-02-24 21:56:57 -0300

    adição de comentarios no codigo

--- Javascript/LoginECadastroContaBanco/LoginECadastroContaBanco.js
+++ Javascript/LoginECadastroContaBanco/LoginECadastroContaBanco.js
@@ -1,30 +1,31 @@
-const readlineSync = require('readline-sync');
+const readlineSync = require('readline-sync'); // utilizando readline para utilizar entrada de dados
 
-// Exemplo: solicitando o nome do usuário
-let nome = readlineSync.question('Digite seu nome: ');
-console.log(`Olá, ${nome}!`);
+let nome = readlineSync.question ('Digite seu nome: '); // readlineSync.question é tipo Input 
+console.log(`Olá, ${nome}!`); // ${nome} é para pegar o nome do input e colocar no console
 
-// Exemplo de uso conforme o código anterior:
-function login() {
+function login() { // funcition é usado para separar codigo por função. Nesse caso logina está dentro de fuction
     const agencia = readlineSync.question("Digite sua agência: ");
     const conta = readlineSync.question("Digite sua conta: ");
 
     if (agencia === "3277" && conta === "3266") {
         console.log("Acesso permitido");
     } else {
-        let criarConta = readlineSync.keyInYNStrict("Agência ou conta incorreta. Gostaria de criar uma conta?");
-        if (criarConta) {
+        let criarConta = readlineSync.keyInYNStrict("Agência ou conta incorreta. Gostaria de criar uma conta?"); // aqui ele abre opção y ou n que ja é nativa da biblioteca
+
+        if (criarConta) { // se for sim, vai ser redirecionado para opção cadastro
             cadastro();
+
         } else {
             console.log("Operação cancelada.");
         }
     }
 }
 
-function cadastro() {
+function cadastro() { // mesmo acontece com cadastro, que está dentro de function que sepera o codigo do login
     console.log("Redirecionando para a página de criação de conta...");
 
-    setTimeout(() => {
+    setTimeout(() => { // aqui ta dando entrada para o tempo de 3 segundos
+
         let nome = readlineSync.question("Digite seu nome: ");
         let cpf = readlineSync.question("Digite seu CPF: ");
         let email = readlineSync.question("Digite seu email: ");
@@ -37,12 +38,10 @@ function cadastro() {
             setTimeout(() => {
                 console.log("Redirecionando para o login...");
                 login();
-            }, 2000);
+            }, 2000); // e aqui fecha
         } else {
             console.log("Erro: as senhas não coincidem.");
         }
     }, 3000);
 }
-
-// Inicia o fluxo de login
-login();
+login(); // e dps de completo o cadastro, ele volta para função login
